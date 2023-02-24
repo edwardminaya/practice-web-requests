b1a822d726a9a38a13a030ddebda49a852264210
“Edward <eminaya49@gmail.com>
2023-02-24 14:28:50 -0600

    created programs with web request

---
+++ dictionary.rb
@@ -0,0 +1,31 @@
+api_key = "YOURAPIKEY"
+require "HTTP"
+
+while true
+  system "clear"
+
+  puts "Enter a word: "
+
+  input_word = gets.chomp
+  response = HTTP.get ("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{api_key}")
+
+  definitions = response.parse(:json)
+
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=#{api_key}")
+  examples = response.parse(:json)
+
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=50&api_key=#{api_key}")
+
+  pronunciations = response.parse(:json)
+
+  pp definitions[1]["text"]
+  pp examples["examples"][0]["text"]
+  pp pronunciations[0]["raw"]
+
+  puts "Enter 'Q' to quite or any other key to continue: "
+  input_option = gets.chomp
+  if input_option.upcase == "Q"
+    puts "Thank you, Goodbye!"
+    break
+  end
+end
---
+++ word_usage.rb
@@ -0,0 +1,42 @@
+api_key = "YOURAPIKEY"
+require "http"
+
+while true
+  system "clear"
+  puts "COMPARE WORDS"
+  puts "Type 'Q' if you would like to exit otherwise type 'Y': "
+  start = gets.chomp
+  if start.upcase == "Q"
+    break
+  end
+
+  puts "Enter two words to compare: "
+
+  user_entries = []
+  2.times do
+    word = gets.chomp
+    user_entries << word
+  end
+
+  puts "Start Year: "
+  start_year = gets.chomp.to_i
+  puts "End Year: "
+  end_year = gets.chomp.to_i
+
+  responses = []
+  i = 0
+  2.times do
+    word = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_entries[i]}/frequency?useCanonical=false&startYear=#{start_year}&endYear=#{end_year}&api_key=#{api_key}")
+    word = word.parse(:json)
+    i += 1
+    responses << word
+  end
+
+  i = start_year
+  k = 0
+  while i <= end_year
+    p "In #{responses[0]["frequency"][k]["year"]} #{user_entries[0]} was used #{responses[0]["frequency"][k]["count"]} and #{user_entries[1]} was used #{responses[1]["frequency"][k]["count"]}"
+    i += 1
+    k += 1
+  end
+end
